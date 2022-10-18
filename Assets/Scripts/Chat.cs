c48da7e6716611edacf5ca86637a787774973ce6
Kiwi <59049734+WillyTrek19@users.noreply.github.com>
2022-10-18 20:24:04 +0200

    Pseudocode UI

---
+++ Assets/Scripts/Chat.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Chat : MonoBehaviour
+{
+    public Text txt;
+    public InputField input;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (input.text != "")
+            {
+                string msg = input.text;
+                input.text = "";
+
+                SendMsg(msg);
+            }
+        }
+    }
+    void SendMsg(string msg)
+    {
+        //This is where user should send message to server
+        RecieveMsg(msg);
+    }
+
+    void RecieveMsg(string msg)
+    {
+        //This is where server sends messages to users
+        txt.text += "\n>>" + msg;
+    }
+}
---
+++ Assets/Scripts/LobbyScripts.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyScripts : MonoBehaviour
+{
+    private static string input;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    public void Go2Create()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Go2Join()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void ReadStringInput(string s)
+    {
+        input = s;
+        Debug.Log("New name: " + input);
+    }
+
+    public void StartServer()
+    {
+        Debug.Log("Created server: " + input);
+        SceneManager.LoadScene(3);
+    }
+
+    public void JoinServer()
+    {
+        Debug.Log("Joined server: " + input);
+        SceneManager.LoadScene(3);
+    }
+}
