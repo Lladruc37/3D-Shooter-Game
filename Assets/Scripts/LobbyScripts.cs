dffe8acb67499448112861076dafd1b654e2d2bc
Kiwi <59049734+WillyTrek19@users.noreply.github.com>
2022-10-19 17:26:44 +0200

    Moved scenes & event management around

--- Assets/Scripts/LobbyScripts.cs
+++ Assets/Scripts/LobbyScripts.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LobbyScripts : MonoBehaviour
 {
-    private static string input;
-
+    //UI
+    public Text title;
+    public Text input;
+    public Canvas inputCanvas;
+    public Canvas chatCanvas;
     private void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
+    {}
     public void Go2Create()
     {
         SceneManager.LoadScene(1);
@@ -22,19 +27,23 @@ public class LobbyScripts : MonoBehaviour
     }
     public void ReadStringInput(string s)
     {
-        input = s;
-        Debug.Log("New name: " + input);
+        input.text = s;
+        Debug.Log("New name: " + input.text);
     }
 
     public void StartServer()
     {
-        Debug.Log("Created server: " + input);
-        SceneManager.LoadScene(3);
+        Debug.Log("Created server: " + input.text);
+        title.text = "Welcome to " + input.text + "! IP: " + "";
+        inputCanvas.GetComponent<Canvas>().enabled = false;
+        chatCanvas.GetComponent<Canvas>().enabled = true;
     }
 
     public void JoinServer()
     {
-        Debug.Log("Joined server: " + input);
-        SceneManager.LoadScene(3);
+        Debug.Log("Joined server: " + input.text);
+        title.text = "Welcome to " + "" + "! IP: " + input.text;
+        inputCanvas.GetComponent<Canvas>().enabled = false;
+        chatCanvas.GetComponent<Canvas>().enabled = true;
     }
 }
